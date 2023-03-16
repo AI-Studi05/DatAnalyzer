535a490b54e870ab9ed1929d206e74ce1e0a426e
Loic Fracheboud <fracheboud@gap-engineering.ch>
2023-03-16 20:42:35 +0100

    ADD: conf.py, CHG: fixed index toctree

---
+++ docs/conf.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+# configuration file for the sphinx documentation
+# all doc available at
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+
+project = 'DatAnalyzer'
+copyright = '2023, Fracheboud Loic, Poncin Clément'
+author = 'Fracheboud Loic, Poncin Clément'
+
+# -- General configuration ---------------------------------------------------
+
+# root path
+sys.path.append(os.path.abspath('..'))
+
+extensions = [
+    'sphinx.ext.autodoc',
+    "sphinx_rtd_theme"
+    ]
+
+exclude_patterns = ["README.rst"]
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "sphinx_rtd_theme"
