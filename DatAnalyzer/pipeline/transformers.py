77d4a64df1a790183486cad7b8b13d8283e815bd
Loic Fracheboud <fracheboud@gap-engineering.ch>
2023-03-20 19:05:13 +0100

    FIX: linter. CHG: pre-commit yaml to integrate our guidelines

--- DatAnalyzer/pipeline/transformers.py
+++ DatAnalyzer/pipeline/transformers.py
@@ -1,23 +1,23 @@
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, PolynomialFeatures
 
-class SmoothData(StandardScaler,MinMaxScaler,PolynomialFeatures):
-    def __init__(self,smooth_type):
-        self.smooth_type=smooth_type
+
+class SmoothData(StandardScaler, MinMaxScaler, PolynomialFeatures):
+    def __init__(self, smooth_type):
+        self.smooth_type = smooth_type
 
     def type_of_preprocess(self):
         if self.smooth_type == "min-max scaling":
-            scaler=MinMaxScaler()
+            scaler = MinMaxScaler()
         elif self.smooth_type == "z-normalisation":
-            scaler=StandardScaler()
+            scaler = StandardScaler()
         elif self.smooth_type == "polynomial features":
-            scaler=PolynomialFeatures()
+            scaler = PolynomialFeatures()
         else:
             raise ValueError("The preprocessing method is not supported")
         return scaler
 
-    def fit(self,X,y=None):
+    def fit(self, X, y=None):
         return self
 
-    def transform(self,X):
+    def transform(self, X):
         return self.transform(X)
