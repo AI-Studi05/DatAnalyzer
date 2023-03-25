bd47f86c8cf79b507e7aeb4569dd697e0416ac3d
Loic Fracheboud <fracheboud@gap-engineering.ch>
2023-03-25 21:51:13 +0100

    CHG: updated module structure and sphinxed doctrings

--- DatAnalyzer/pipeline/transformers.py
+++ DatAnalyzer/pipeline/transformers.py
@@ -4,10 +4,25 @@ from sklearn.preprocessing import PolynomialFeatures
 
 
 class SmoothData(StandardScaler, MinMaxScaler, PolynomialFeatures):
+    """
+    This class is used to smooth the data using different methods
+    """
+
     def __init__(self, smooth_type):
+        """
+        With the provided string, extract the path and the extension.
+
+        :param str smooth_type: Type of smoothing
+        """
         self.smooth_type = smooth_type
 
     def type_of_preprocess(self):
+        """
+        Return the type of preprocessing
+
+        :return: Type of preprocessing
+        :rtype: str
+        """
         if self.smooth_type == "min-max scaling":
             scaler = MinMaxScaler()
         elif self.smooth_type == "z-normalisation":
@@ -19,7 +34,24 @@ class SmoothData(StandardScaler, MinMaxScaler, PolynomialFeatures):
         return scaler
 
     def fit(self, X, y=None):
+        """
+        Fit the data
+
+        :param X: Data to fit
+        :param y: Target
+
+        :return: self
+        :rtype: SmoothData
+        """
         return self
 
     def transform(self, X):
+        """
+        Transform the data
+
+        :param X: Data to transform
+
+        :return: Transformed data
+        :rtype: numpy.ndarray
+        """
         return self.transform(X)
