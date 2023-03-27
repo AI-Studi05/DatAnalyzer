bc454479648c69ebf23d3eb79e6baf78c5f495d4
Clément Poncin <poncin.clement6@gmail.com>
2023-03-27 17:56:09 +0200

    [ADD] add a test on the preprocessing method.

--- test/test_.py
+++ test/test_.py
@@ -1,5 +1,6 @@
 import pytest
 from DatAnalyzer.pipeline.formatters import Data_Formating
+from DatAnalyzer.pipeline.transformers import SmoothData
 
 
 def test_import_data_with_wrong_extension():
@@ -7,3 +8,10 @@ def test_import_data_with_wrong_extension():
     data_format = Data_Formating(path)
     with pytest.raises(ValueError):
         data_format.import_data()
+
+
+def test_preprocessing_with_wrong_preprocessed_method():
+    method = "log scaling"
+    data_format = SmoothData(method)
+    with pytest.raises(ValueError):
+        data_format.type_of_preprocess()
