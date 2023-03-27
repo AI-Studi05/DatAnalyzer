076613eb131e80408a5612ba600304fe6533a101
Clément Poncin <poncin.clement6@gmail.com>
2023-03-27 17:48:38 +0200

    [ADD] unit test that test the extension

--- test_.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from DatAnalyzer.pipeline.formatters import Data_Formating
-
-
-def test_import_data_with_wrong_extension():
-    path = "test_data.txt"
-    data_format = Data_Formating(path)
-    with pytest.raises(ValueError):
-        data_format.import_data()
